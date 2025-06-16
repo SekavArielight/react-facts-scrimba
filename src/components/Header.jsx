a68964520b2e5445ec433cd6fcf6ed007e00e9b4
God'swill Sekav Apya <godswillapya@gmail.com>
2025-06-16 17:32:35 +0100

    Refactor Header component structure and update CSS for improved layout and styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,18 +3,9 @@ import React from "react";
 const Header = () => {
   return (
     <header>
-      <img
-        src="../public/react-logo.png"
-        alt="React Logo"
-        className="logo"
-      />
-
       <nav>
-        <ul className="nav-list">
-          <li>Pricing</li>
-          <li>About</li>
-          <li>Contact</li>
-        </ul>
+        <img src="../public/react-logo.png" alt="React Logo" className="logo" />
+        <span>ReactFacts</span>
       </nav>
     </header>
   );
